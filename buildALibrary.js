0b0d687b38e2d2cc9e762580888d9bb7b9c3d02c
Nargis <nargis.salim@btinternet.com>
2021-07-03 10:27:25 +0100

    first commit

---
+++ buildALibrary.js
@@ -0,0 +1,5 @@
+// Build a Library
+// To modernise a library which is still using index cards to organize their content! 
+
+// Library carries three different types of media: books, CDs, and movies.
+
